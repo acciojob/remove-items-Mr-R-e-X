69eea5c9eafee5833612f6e476c8a578f8deff12
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-28 15:04:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function removeSelectedOption() {
+  let options = document.querySelector("#colorSelect");
+  let optionIndex = options.selectedIndex;
+  if (optionIndex !== -1) {
+    options.remove(optionIndex);
+  }
+}
